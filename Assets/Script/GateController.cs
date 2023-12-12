17da9c0fa0c4da8cde736346822f2e1b10fb26f7
Eray Yuksel <yukseleray203@gmail.com>
2023-12-12 17:56:42 +0300

    BulletStart

--- Assets/Script/GateController.cs
+++ Assets/Script/GateController.cs
@@ -8,23 +8,52 @@ public class GateController : MonoBehaviour
     public enum GateType { Power, Range, FireRate, Count }
     public GateType gateType;
     [SerializeField] TextMeshProUGUI GateText;
-    [SerializeField] int currentValue = 2;
+    [SerializeField] int currentValue;
+    [SerializeField] GameObject glassGO;
+    Renderer glassRenderer;
+    [SerializeField] Material[] materials;
+    bool isPassed = true;
 
+    private void Start()
+    {
+        glassRenderer = glassGO.GetComponent<Renderer>();
+        GateStart();
+        GateCheck();
+    }
     void GateStart()
     {
         switch (gateType)
         {
             case GateType.Power:
-                GateText.text = "+" + currentValue.ToString();
+                GateText.text = currentValue.ToString();
                 break;
             case GateType.Range:
+                GateText.text = currentValue.ToString();
                 break;
             case GateType.FireRate:
-                break;
-            case GateType.Count:
+                GateText.text = currentValue.ToString();
                 break;
             default:
                 break;
+        }
+    }
+    void GateCheck()
+    {
+        if (currentValue < 0)
+        {
+            glassRenderer.material = materials[0];
+        }
+        if (currentValue > 0)
+        {
+            glassRenderer.material = materials[1];
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isPassed == true)
+        {
+            isPassed = false;
+            Debug.Log("Gate Passed");
         }
     }
 }
